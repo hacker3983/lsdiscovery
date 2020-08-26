fae781cbe7fcb368ebdfcfac0bf605905df69c98
hacker3983 <54384337+hacker3983@users.noreply.github.com>
2020-08-26 14:49:56 -0400

    Update checkupdate.sh

--- checkupdate.sh
+++ checkupdate.sh
@@ -3,3 +3,9 @@ rm -rR ls-discovery
 git clone https://github.com/cyberinventor/ls-discovery.git
 cd ls-discovery
 bash install.sh
+clear
+echo "update complete"
+echo "your terminal will be closes down in 10 seconds to apply the update or the changes that have been made."
+echo "after your terminal closes open a next terminal and the update will be applied."
+sleep 10
+echo "closing terminal..."
