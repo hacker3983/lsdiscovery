008e7bccc4ed6cefaa95d7217a7cabe853d111f0
hacker3983 <54384337+hacker3983@users.noreply.github.com>
2020-08-26 16:47:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ echo cloning wordlists
 git clone https://github.com/hacker3983/wordlist.git
 echo "Installation complete"
 echo "copying files from lsdiscovery/wordlist into lsdiscovery"
+bash copy.sh
+python3 copy.py
