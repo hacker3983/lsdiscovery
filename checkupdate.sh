430d5eb441a57ea4ec54de3f9966dc142f008192
hacker3983 <54384337+hacker3983@users.noreply.github.com>
2020-08-26 15:13:17 -0400

    Update checkupdate.sh

--- checkupdate.sh
+++ checkupdate.sh
@@ -1,3 +1,4 @@
+ifprintf '\033]2;updating in progress\a
 cd ..
 rm -rR ls-discovery
 git clone https://github.com/cyberinventor/ls-discovery.git
@@ -9,4 +10,16 @@ echo "your terminal will be closes down in 10 seconds to apply the update or the
 echo "after your terminal closes open a next terminal and the update will be applied."
 sleep 10
 echo "closing terminal..."
+echo "trying to close"
 killall qterminal
+echo "trying to close"
+killall gnome-terminal
+echo "trying to close"
+killall xterm
+echo "trying to close"
+killall uxterm
+echo "trying to close"
+killall "1699 votes"
+echo "trying to close"
+killall "guake"
+echo "if terminal does not close i will find a better way to do it."
