4739a6cfc7dd4eaa91f9253483ec78040088a383
hacker3983 <54384337+hacker3983@users.noreply.github.com>
2020-08-26 13:45:01 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 apt-get install python3-pip
-apt-get install cowsay
 pip3 install -r requirements.txt
+apt-get install git
+echo cloning wordlists
+git clone https://github.com/hacker3983/wordlist.git
