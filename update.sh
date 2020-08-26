7d3fc6c21441cc1802842f9d857695f5d264523f
hacker3983 <54384337+hacker3983@users.noreply.github.com>
2020-08-26 18:11:38 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,44 +5,4 @@ git clone https://github.com/cyberinventor/ls-discovery.git
 cd ls-discovery
 bash install.sh
 clear
-echo "update complete"
-echo "your terminal will be closes down in 10 seconds to apply the update or the changes that have been made."
-echo "after your terminal closes open a next one and update will be applied"
-sleep 10
-echo "closing terminal..."
-echo "closing"
-sleep 10
-ps -p $$ -o ppid=qterminal | tee output.txt
-python3 close.py
-
-a = "gnome-terminal"
-ps -p $$ -o ppid="$a" | tee output.txt
-python3 close.py
-
-b = "guake"
-ps -p $$ -o ppid="$b" | tee output.txt
-python3 close.py
-
-c = "konsole"
-ps -p $$ -o ppid="$c" | tee output.txt
-python3 close.py
-
-d = "terminator"
-ps -p $$ -o ppid="$d" | tee output.txt
-python3 close.py
-
-e = "tilda"
-ps -p $$ -o ppid="$e" | tee output.txt
-python3 close.py
-
-f = "xterm"
-ps -p $$ -o ppid="$f" | tee output.txt
-python3 close.py
-
-g = "yakuas"
-ps -p $$ -o ppid="$g" | tee output.txt
-python3 close.py
-
-h = "tilix"
-ps -p $$ -o ppid="$h" | tee output.txt
-python3 close.py
+echo "update complete now copy and paste the text in green and press enter"
