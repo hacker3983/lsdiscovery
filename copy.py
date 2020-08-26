06200b050c5ba4c5b6bf346da95ed4933fce51cb
hacker3983 <54384337+hacker3983@users.noreply.github.com>
2020-08-26 18:07:28 -0400

    Delete copy.py

--- copy.py
+++
@@ -1,5 +0,0 @@
-import os
-
-username = open('username.txt')
-os.system("cd wordlist && cp apache-user-enum-1.0.txt  default-list-2.3-medium.txt  directory-list-1.0.txt directory-list-lowercase-2.3-medium.txt /"+username.readline()+"/ls-discovery")
-os.system("cd wordlist && cp apache-user-enum-2.0.txt  directories.jbrofuzz directory-list-2.3-small.txt  directory-list-lowercase-2.3-small.txt  /"+username.readline()+"/ls-discovery")
