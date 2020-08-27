25ba769453b6a8f361e7d6aac643a5b5100a5c90
hacker3983 <54384337+hacker3983@users.noreply.github.com>
2020-08-26 22:44:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 printf '\033]2;Installation in progress\a'
-sudo apt-get install python3 -y
-sudo apt-get install python3-pip -y
+sudo apt-get install python3
+wget https://github.com/hacker3983/wordlist/raw/master/default-list-2.3-medium.txt
+wget https://raw.githubusercontent.com/hacker3983/wordlist/master/apache-user-enum-1.0.txt
+wget https://raw.githubusercontent.com/hacker3983/wordlist/master/apache-user-enum-2.0.txt
+wget https://raw.githubusercontent.com/hacker3983/wordlist/master/directory-list-1.0.txt
+wget https://github.com/hacker3983/wordlist/raw/master/directories.jbrofuzz
+wget https://raw.githubusercontent.com/hacker3983/wordlist/master/directory-list-2.3-small.txt
+wget https://raw.githubusercontent.com/hacker3983/wordlist/master/directory-list-lowercase-2.3-medium.txt
+wget https://github.com/hacker3983/wordlist/blob/master/directory-list-lowercase-2.3-small.txt
+sudo apt-get install python3-pip
 pip3 install -r requirements.txt
-sudo apt-get install git -y
+sudo apt-get install git
 echo cloning wordlists
-git clone https://github.com/hacker3983/wordlist.git
 echo "Installation complete"
