14399a1bb2f61101d11420976179ebec2496a1e2
hacker3983 <54384337+hacker3983@users.noreply.github.com>
2020-08-26 20:20:13 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 printf '\033]2;Installation in progress\a'
-sudo apt-get install python3
-sudo apt-get install python3-pip
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
 pip3 install -r requirements.txt
-sudo apt-get install git
+sudo apt-get install git -y
 echo cloning wordlists
 git clone https://github.com/hacker3983/wordlist.git
 echo "Installation complete"
